46accfa8ce9faf618b0f39de141994722f7837ba
Felicio Angga <211110176@students.mikroskil.ac.id>
2025-06-03 22:52:33 +0700

    add read write in access right

--- src/features/access-rights/components/AccessRightModal.tsx
+++ src/features/access-rights/components/AccessRightModal.tsx
@@ -3,6 +3,7 @@ import Form from '../../../components/Form';
 import { useForm } from 'react-hook-form';
 import { Input } from '../../../components/input/Input';
 import Checkbox from '../../../components/Checkbox';
+import Button from '../../../components/Button';
 
 interface AccessRightModalProps {
   isOpen: boolean;
@@ -40,26 +41,87 @@ function AccessRightModal({ isOpen, onClose }: AccessRightModalProps) {
             <div className="pl-6">
               <p className="font-medium mt-2">Student Management</p>
               <div className="pl-6 mt-2 flex flex-col gap-2">
-                <Checkbox label="Registration" />
-                <Checkbox label="Student Data" />
-                <Checkbox label="Classes" />
-                <Checkbox label="Absence" />
+                <div className="flex justify-between">
+                  <Checkbox label="Registration" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <Checkbox label="Student Data" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <Checkbox label="Absence" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
               </div>
               <p className="font-medium mt-2">Curriculum</p>
               <div className="pl-6 mt-2 flex flex-col gap-2">
-                <Checkbox label="Subject" />
+                <div className="flex justify-between">
+                  <Checkbox label="Academic" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <Checkbox label="Class Note" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
+                
                 <Checkbox label="Class Schedule" />
               </div>
               <p className="font-medium mt-2">Reporting</p>
               <div className="pl-6 mt-2 flex flex-col gap-2">
-                <Checkbox label="Absence Recap" />
-                <Checkbox label="Exam Recap" />
+                <div className="flex justify-between">
+                  <Checkbox label="Absence Recap" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <Checkbox label="Exam Recap" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
               </div>
               <p className="font-medium mt-2">Configuration</p>
               <div className="pl-6 mt-2 flex flex-col gap-2">
-                <Checkbox label="Grade" />
-                <Checkbox label="Classes" />
-                <Checkbox label="Classes Schedule" />
+                <div className="flex justify-between">
+                  <Checkbox label="Grade" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <Checkbox label="Classes" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <Checkbox label="Subject" />
+                  <div className="flex gap-3">
+                    <Checkbox label="Read" />
+                    <Checkbox label="Write" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -67,35 +129,94 @@ function AccessRightModal({ isOpen, onClose }: AccessRightModalProps) {
           <div className="mt-3">
             <p className="font-medium">Archive</p>
             <div className="pl-6 mt-2 flex flex-col gap-2">
-              <Checkbox label="Document" />
-              <Checkbox label="Document Type" />
+              <div className="flex justify-between">
+                <Checkbox label="Document" />
+                <div className="flex gap-3">
+                  <Checkbox label="Read" />
+                  <Checkbox label="Write" />
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <Checkbox label="Document Type" />
+                <div className="flex gap-3">
+                  <Checkbox label="Read" />
+                  <Checkbox label="Write" />
+                </div>
+              </div>
             </div>
           </div>
 
           <div className="mt-3">
             <p className="font-medium">Human Resource</p>
             <div className="pl-6 mt-2 flex flex-col gap-2">
-              <Checkbox label="Teacher" />
-              <Checkbox label="Absence" />
-              <Checkbox label="Absence Recap" />
-              <Checkbox label="Working Schedule" />
+              <div className="flex justify-between">
+                <Checkbox label="Teacher" />
+                <div className="flex gap-3">
+                  <Checkbox label="Read" />
+                  <Checkbox label="Write" />
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <Checkbox label="Absence" />
+                <div className="flex gap-3">
+                  <Checkbox label="Read" />
+                  <Checkbox label="Write" />
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <Checkbox label="Absence Recap" />
+                <div className="flex gap-3">
+                  <Checkbox label="Read" />
+                  <Checkbox label="Write" />
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <Checkbox label="Working Schedule" />
+                <div className="flex gap-3">
+                  <Checkbox label="Read" />
+                  <Checkbox label="Write" />
+                </div>
+              </div>
             </div>
           </div>
 
           <div className="mt-3">
             <p className="font-medium">Website Management</p>
             <div className="pl-6 mt-2 flex flex-col gap-2">
-              <Checkbox label="News & Event" />
+              <div className="flex justify-between">
+                <Checkbox label="News & Event" />
+                <div className="flex gap-3">
+                  <Checkbox label="Read" />
+                  <Checkbox label="Write" />
+                </div>
+              </div>
             </div>
           </div>
 
           <div className="mt-3">
             <p className="font-medium">Settings</p>
             <div className="pl-6 mt-2 flex flex-col gap-2">
-              <Checkbox label="Users" />
-              <Checkbox label="Access Rights" />
+              <div className="flex justify-between">
+                <Checkbox label="Users" />
+                <div className="flex gap-3">
+                  <Checkbox label="Read" />
+                  <Checkbox label="Write" />
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <Checkbox label="Access Rights" />
+                <div className="flex gap-3">
+                  <Checkbox label="Read" />
+                  <Checkbox label="Write" />
+                </div>
+              </div>
             </div>
           </div>
+        </div>
+
+        <div className="flex gap-5 mt-5">
+          <Button variant="outline" className="w-full">Batal</Button>
+          <Button className="w-full">Simpan</Button>
         </div>
       </Form>
     </Modal>
