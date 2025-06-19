c8ea2528e90323aed57edcff550c6e052552e05f
felicio.angga <211110176@students.mikroskil.ac.id>
2025-06-19 16:22:32 +0700

    add png type to news

--- src/features/news-form/components/CustomInputFile.tsx
+++ src/features/news-form/components/CustomInputFile.tsx
@@ -35,8 +35,8 @@ export const FileUploader = ({ selectedFile, setSelectedFile, previewUrl, setPre
 
   const MAX_FILE_SIZE_MB = 5;
   const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
-  const ALLOWED_FILE_TYPES = ["image/jpeg", "image/jpg"];
-  const ALLOWED_FILE_TYPES_STRING = "JPG & JPEG";
+  const ALLOWED_FILE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
+  const ALLOWED_FILE_TYPES_STRING = "JPG & JPEG & PNG";
 
   const handleFile = (file: File | null) => {
     if (!file) {
