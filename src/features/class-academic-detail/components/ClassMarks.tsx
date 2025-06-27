10351135996a9e552507dbd8694988baaeaa8645
Felicio Angga <211110176@students.mikroskil.ac.id>
2025-06-27 13:15:16 +0700

    fix table in class marks

--- src/features/class-academic-detail/components/ClassMarks.tsx
+++ src/features/class-academic-detail/components/ClassMarks.tsx
@@ -377,7 +377,7 @@ function ClassMarks({ setTermId, termId }: ClassMarksProps) {
                       })}
                     </tr>
                   ))}
-                  {getPermissionAccess("academic_all_score").write && (
+                  {getPermissionAccess("academic_all_score").write ? (
                     <tr>
                       <td className="border border-gray-400 px-3 py-2 align-top">
                         <div className="flex gap-2 items-center">
@@ -390,7 +390,7 @@ function ClassMarks({ setTermId, termId }: ClassMarksProps) {
                         : studentNotes.find(note => note.student_id === student.id)?.second_term_notes || ""}
                       </td>
                     </tr>
-                  )}
+                  ) : <tr></tr>}
                 </React.Fragment>
               );
             })}
